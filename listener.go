79966d792ea6c5161a64b55b6c25bf8586b71ece
bhenderson <bhenderson@yp.com>
2012-10-05 14:14:42 -0700

    Got some of the fields working.

--- listener.go
+++ listener.go
@@ -86,6 +86,18 @@ func deserializeEvent(event *Event, buf []byte) {
     var attrSize uint16
     binary.Read(p, binary.BigEndian, &attrSize)
 
+    // temporary types
+    // var tmpByte   byte
+    // var tmpUint16 uint16
+    // var tmpInt16  int16
+    // var tmpUint32 uint32
+    // var tmpInt32  int32
+    // var tmpUint64 uint64
+    // var tmpInt64  int64
+    // var tmpBool   byte
+    // var tmpIpaddr net.IP
+    // var tmpString string
+
     for i:=0; i < int(attrSize); i++ {
         var attrNameSize byte
         var attrName string
@@ -96,18 +108,36 @@ func deserializeEvent(event *Event, buf []byte) {
 
         binary.Read(p, binary.BigEndian, &attrType)
 
-        log.Println(attrName, attrType)
+        // log.Println(attrName, attrType)
 
         switch int(attrType) {
-        case 1: // LWES_U_INT_16_TOKEN
-        case 2: // LWES_INT_16_TOKEN
-        case 3: // LWES_U_INT_32_TOKEN
+        // case 1: // LWES_U_INT_16_TOKEN
+            // binary.Read(p, binary.BigEndian, &tmpUint16)
+            // event.attributes[attrName] = uint16(p.Next(int(tmpUint16)))
+        // case 2: // LWES_INT_16_TOKEN
+            // binary.Read(p, binary.BigEndian, &tmpInt16)
+            // event.attributes[attrName] = int16(p.Next(int(tmpInt16)))
+        // case 3: // LWES_U_INT_32_TOKEN
+            // binary.Read(p, binary.BigEndian, &tmpUint32)
+            // event.attributes[attrName] = uint32(p.Next(int(tmpUint32)))
         case 4: // LWES_INT_32_TOKEN
+            event.attributes[attrName] = p.Next(4)
         case 5: // LWES_STRING_TOKEN
+            var size uint16
+            binary.Read(p, binary.BigEndian, &size)
+            event.attributes[attrName] = string(p.Next(int(size)))
         case 6: // LWES_IP_ADDR_TOKEN
-        case 7: // LWES_INT_64_TOKEN
-        case 8: // LWES_U_INT_64_TOKEN
+            tmpIp := p.Next(4)
+            // not user if this is completely accurate
+            event.attributes[attrName] = net.IPv4(tmpIp[3], tmpIp[2], tmpIp[1], tmpIp[0])
+        // case 7: // LWES_INT_64_TOKEN
+            // binary.Read(p, binary.BigEndian, &tmpInt64)
+            // event.attributes[attrName] = int64(p.Next(int(tmpInt64)))
+        // case 8: // LWES_U_INT_64_TOKEN
+            // binary.Read(p, binary.BigEndian, &tmpUint64)
+            // event.attributes[attrName] = uint64(p.Next(int(tmpUint64)))
         case 9: // LWES_BOOLEAN_TOKEN
+            event.attributes[attrName] = 1 == p.Next(1)[0]
         }
     }
 }
