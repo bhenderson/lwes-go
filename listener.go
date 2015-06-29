97d32b61b3869651f219acdb0e3c019d6c8d70a2
bhenderson <henderson.bj@gmail.com>
2015-06-28 19:18:13 -0700

    Remove listenerAction
    I think clients should just use channels if they want something like
    this.

--- listener.go
+++ listener.go
@@ -10,25 +10,12 @@ type Listener struct {
 	socket *Conn
 }
 
-type listenerAction func(*Event, error) error
-
 // NewListener creates a new Listener and binds to ip and port and iface
 func NewListener(udp string, iface ...*net.Interface) (*Listener, error) {
 	conn, err := NewConn(udp, false, iface...)
 	l := &Listener{socket: conn}
 
 	return l, err
-}
-
-// Each takes a listenerAction and gives it an *Event. See listenerAction.
-func (l *Listener) Each(action listenerAction) {
-	var err error
-	for {
-		err = action(l.Recv())
-		if err != nil {
-			panic(err)
-		}
-	}
 }
 
 // Recv receives an event
--- lwes-listener/lwes-listener.go
+++ lwes-listener/lwes-listener.go
@@ -1,71 +1,74 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "github.com/bhenderson/lwes-go"
-    "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/bhenderson/lwes-go"
 )
 
 var (
-    addr string
-    pretty bool
-    printj bool
+	addr   string
+	pretty bool
+	printj bool
 )
 
 func init() {
-    flag.Usage = usage
+	flag.Usage = usage
 
-    flag.StringVar(&addr, "address", "224.2.2.22:12345", "Listen Channel")
-    flag.BoolVar(&pretty, "pretty", false, "Pretty print event")
-    flag.BoolVar(&printj, "json", false, "Print event as json")
+	flag.StringVar(&addr, "address", "224.2.2.22:12345", "Listen Channel")
+	flag.BoolVar(&pretty, "pretty", false, "Pretty print event")
+	flag.BoolVar(&printj, "json", false, "Print event as json")
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    listener, err := lwes.NewListener(addr)
+	listener, err := lwes.NewListener(addr)
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    listener.Each(callback)
+	for {
+		callback(listener.Recv())
+	}
 }
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "Usage: %s [opts]\n", os.Args[0])
-    flag.PrintDefaults()
-    os.Exit(1)
+	fmt.Fprintf(os.Stderr, "Usage: %s [opts]\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(1)
 }
 
 func callback(event *lwes.Event, err error) error {
-    if err != nil {
-        fmt.Println(err)
-        return nil
-    }
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 
-    switch {
-    default:
-        fmt.Println(event)
-    case printj:
-        var b []byte
-        if pretty {
-            b, _ = json.MarshalIndent(event, "", "  ")
-        } else {
-            b, _ = json.Marshal(event)
-        }
-        fmt.Println(string(b))
-    case pretty:
-        fmt.Println(event.Name)
-        for k, _ := range event.Attributes {
-            fmt.Printf("%s: %v\n", k, event.Attributes[k])
-        }
-        fmt.Println("")
-    }
-    os.Stdout.Sync()
+	switch {
+	default:
+		fmt.Println(event)
+	case printj:
+		var b []byte
+		if pretty {
+			b, _ = json.MarshalIndent(event, "", "  ")
+		} else {
+			b, _ = json.Marshal(event)
+		}
+		fmt.Println(string(b))
+	case pretty:
+		fmt.Println(event.Name)
+		for k, _ := range event.Attributes {
+			fmt.Printf("%s: %v\n", k, event.Attributes[k])
+		}
+		fmt.Println("")
+	}
+	os.Stdout.Sync()
 
-    return nil
+	return nil
 }
