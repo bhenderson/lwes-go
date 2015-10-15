65dc4818dc81f79849925a42b812a3366ea0e2d4
bhenderson <henderson.bj@gmail.com>
2015-10-15 11:09:22 -0700

    Format emitter.

--- lwes-emitter/lwes-emitter.go
+++ lwes-emitter/lwes-emitter.go
@@ -1,71 +1,71 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "github.com/bhenderson/lwes-go"
-    "io"
-    "log"
-    "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/bhenderson/lwes-go"
 )
 
 var (
-    addr string
-    emitter *lwes.Emitter
+	addr    = flag.String("address", "224.2.2.22:12345", "Listen Channel")
+	emitter *lwes.Emitter
 )
 
 func init() {
-    flag.Usage = usage
+	flag.Usage = usage
 
-    flag.StringVar(&addr, "address", "224.2.2.22:12345", "Listen Channel")
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    var err error
-    emitter, err = lwes.NewEmitter(addr)
+	var err error
+	emitter, err = lwes.NewEmitter(*addr)
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    emitter.Heartbeat = 0
+	emitter.Heartbeat = 0
 
-    fromJson()
+	fromJson()
 }
 
 func emit(e *lwes.Event) {
-    err := emitter.Emit(e)
+	err := emitter.Emit(e)
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func fromJson() {
-    e := lwes.NewEvent()
+	e := lwes.NewEvent()
 
-    dec := json.NewDecoder(os.Stdin)
+	dec := json.NewDecoder(os.Stdin)
 
-    for {
+	for {
 
-        if err := dec.Decode(e); err == io.EOF {
-            break
-        } else if err != nil {
-            log.Fatal("json error: ", err)
-        }
+		if err := dec.Decode(e); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal("json error: ", err)
+		}
 
-        emit(e)
-    }
+		emit(e)
+	}
 }
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "Usage: json_input | %s [opts]\n", os.Args[0])
-    flag.PrintDefaults()
-    fmt.Print("json_input optionally can contain Name.\n")
-    os.Exit(1)
+	fmt.Fprintf(os.Stderr, "Usage: json_input | %s [opts]\n", os.Args[0])
+	flag.PrintDefaults()
+	fmt.Print("json_input optionally can contain Name.\n")
+	os.Exit(1)
 }
